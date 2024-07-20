285ec46906435ab26188531b3b282af50338b6fa
Hamza607 <101787339+Hamza607@users.noreply.github.com>
2024-07-20 12:40:54 +0500

    remove the input package

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,15 @@ import Footer from "./components/footer/Footer";
 import KasperSky from "./components/kaspersky/KasperSky";
 import PepsiSection from "./components/pepsiSection/PepsiSection";
 import SunnySide from "./components/sunnyside/SunnySide";
-import Adidas_4D from './components/Adidas_4D/Adidas_4D'
-import Architects from './components/Architects/Architects'
-import David from './components/David/David'
-import Inputtype from "./components/Inputtype";
+import Adidas_4D from "./components/Adidas_4D/Adidas_4D";
+import Architects from "./components/Architects/Architects";
+import David from "./components/David/David";
 
 function App() {
   return (
     <>
-        <Header />
-        {/* <Inputtype /> */}
-      <div className="overflow-hidden w-full  mx-auto" >
+      <Header />
+      <div className="overflow-hidden w-full  mx-auto">
         <HeroSection />
         <Adidas />
         <SamsungSection />
@@ -26,8 +24,8 @@ function App() {
         <PepsiSection />
         <SunnySide />
         <Adidas_4D />
-      <Architects/>
-      <David/>
+        <Architects />
+        <David />
         <Tennents />
 
         <Footer />
